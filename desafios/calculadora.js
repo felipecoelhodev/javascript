8c560b55bd0c5f1c99794b385a6fdfd7d044f68b
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-01-12 11:55:52 -0300

    first commit

---
+++ desafios/calculadora.js
@@ -0,0 +1,34 @@
+// calculadora.js
+// Rodar com: node calculadora.js
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log("Bem-vindo(a) à Calculadora do Terminal!\n");
+
+// Passo 1: pedir o primeiro número
+rl.question("Digite o primeiro número: ", (input1) => {
+    const numero1 = Number(input1);
+
+    // TODO: verificar se numero1 é um número válido
+
+    // Passo 2: pedir o operador
+    rl.question("Digite o operador (+, -, *, /): ", (operador) => {
+        // TODO: verificar se o operador é válido
+
+        // Passo 3: pedir o segundo número
+        rl.question("Digite o segundo número: ", (input2) => {
+            const numero2 = Number(input2);
+
+            // TODO: verificar se numero2 é um número válido
+
+            // TODO: fazer a operação de acordo com o operador
+
+            // TODO: mostrar o resultado e encerrar o programa com rl.close()
+        });
+    });
+});
---
+++ variaveis.js
@@ -0,0 +1,17 @@
+var idade = 36;
+console.log("Idade com var:", idade);
+
+idade = 37;
+console.log("Fiz aniversário! Idade agora é:", idade);
+
+let altura = 1.75;
+console.log("Altura:", altura);
+
+altura = 1.76;
+console.log("Cresci! Altura agora é:", altura);
+
+const nome = "Vinicios";
+
+// nome = 'Marcos'
+
+console.log("Nome:", nome);
